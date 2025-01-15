e55169ce2c3210f4b381934e2edd52c50f320638
fredcw <fredcw@nb02Dell.fcwsistemas.com.br>
2025-01-15 15:50:46 -0300

    Geracao de microcodigo em formato IHEX
    para carga do controlador de microcodigo

--- microcodigo.py
+++ microcodigo.py
@@ -1,28 +1,40 @@
 import csv
+from intelhex import IntelHex
+import sys
 
+
+arquivoMicrocodigo = "microcodigo.hex"
+ih = IntelHex()
 
 FMicroCodigo = 'microcodigo.csv'
 
 
-bitsControle = ['PCLD',
-       'PCCNT_e',
-       'PCCOUT_e',
-       'NXT_I',
-       'RInst_E',
-       'DIRECTAddr_e',
-       'ADDRRAM_e',
-       'ACC_E',
-       'ALU_RSLT_e',
-       'ALU_OUT_e',
-       'RAM_e',
-       'IO_e',
-       'ALU_SUB',
-       'ALU_INV',
-       'ALU_CYIN',
-       'WR/~RD'
-       ]
+bitsControle = {
+       0: 'PCLD',
+       1: 'PCCNT_e',
+       2: 'PCCOUT_e',
+       3: 'NXT_I',
+       4: 'RInst_E',
+       5: 'DIRECTAddr_e',
+       6: 'ADDRRAM_e',
+       7: 'ACC_E',
+       8: 'ALU_RSLT_e',
+       9: 'ALU_OUT_e',
+      10: 'RAM_e',
+      11: 'IO_e',
+      12: 'ALU_SUB',
+      13: 'ALU_INV',
+      14: 'ALU_CYIN',
+      15: 'WR/~RD'
+       }
+
+nBitsInstrucao = 4
+# Indice o uCodigo = Codigo da instrucao
+# Total de instrucoes possiveis >> 2^nBitsInstrucao -1 .
+# o RANGE no python é de dominio aberto , ... até (n-1) comecando em 0 ... 
 
 
+F = open('microcodigo.hex','w')
 with open(FMicroCodigo, mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file,  delimiter='\t')
     line_count = 0
@@ -35,9 +47,18 @@ with open(FMicroCodigo, mode='r') as csv_file:
         T = row['Tempo']
         # combina os 16 bits do barramento de controle
         #
-        print( '{:04b}'.format(int(I)) , '{:03b}'.format(int(T)) , ''.join([ row[B] for B in bitsControle   ] ) )
-        
-        
+        uCodigo = [0,0,0]
+        addruInstr = '0b{:04b}{:03b}'.format(int(I),int(T))
+        for BT in range(0,2):
+            bControle = '0b'+''.join([ row[bitsControle[(BT*8)+ndx]] for ndx in range(0,8) ])
+            uCodigo[BT] = int(bControle, 2)
+        print(  int(addruInstr,2) , addruInstr ,'{:04b}'.format(int(I)) ,bControle, '{:03b}'.format(int(T)) , [ '{:02X}'.format(X) for X in uCodigo ] )
+
+        for I in range(len(uCodigo)):
+            ih[int(addruInstr,2)+int(T)*len(uCodigo)+I] = uCodigo[I]
+
+ih.write_hex_file(sys.stdout)
+ih.write_hex_file(arquivoMicrocodigo)
         
         
     
