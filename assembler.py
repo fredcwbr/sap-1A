d7be4f8666adc7ea42dde5646b5ea1b0462a5db4
fredcw <fredcw@nb02Dell.fcwsistemas.com.br>
2025-01-20 17:20:32 -0300

    Teste de JZ - OK
    Adicionado o teste para o JZ. ,

--- assembler.py
+++ assembler.py
@@ -5,15 +5,26 @@ import sys
 import yaml
 import re
 
-cdFonte = 'Sap1A_Ex01.asm'
+
+brancos = {"\t":' ', "\v": ' ' }
+
+
+
+# Fonte do Programa
+if len(sys.argv) > 1:
+    cdFonte = sys.argv[1]
+else:
+    cdFonte = 'Sap1A_Ex02.asm'
+    
+# Fonte do microcodigo
+uCodigoFonte = 'microcodigo.yaml'
+
+
 fResult =  [ 'lst', 'hex' ]
 
 rWords = [ 'DB' ]
 eWords = [ 'EQU', 'ORG' ]
 
-
-# Fonte do microcodigo
-uCodigoFonte = 'microcodigo.yaml'
 with open(uCodigoFonte,'rt', encoding='utf-8') as uC:
     uCodigo = yaml.safe_load(uC)
 
@@ -49,16 +60,33 @@ passos = []
 
 
 
-def monta( seq, instr, operando ):    
+def monta( seq, instr, operando ):
+    # trata operando com possibilidade de informacao 
+    # em hex , octal ou decimal
+    #
+    if not isinstance(operando,int):
+        operando = operando.upper()
+        if 'X' in operando:
+            # converte de hexa.,
+            OPx= int(operando.split('X')[1],16)
+        elif 'O' in operando.upper():
+            # converte de octal.,
+            OPx= int(operando.split('O')[1],8)
+        elif 'B' in operando.upper():
+            # converte de binario.,
+            OPx= int(operando.split('B')[1],2)
+        else:
+            OPx= int(operando)  # Decimal
+    else:
+        OPx = int(operando)
     codObj = '{OP:04X}{INSTR:01X}'.format(
-                OP=int(operando),
+                OP = OPx,
                 INSTR=int(uCodigo['opCodes'][instr]['cdInstrucao'])
                 )
     return codObj, seq, instr, operando ,uCodigo['opCodes'][instr]['cdInstrucao']
 
 def lSplit( strx ):
     x = [None, None, None]
-    # print(strx)
     if strx.strip() == '' or str is None:
         return x
     if ':' in strx:
@@ -66,7 +94,7 @@ def lSplit( strx ):
     else:
         lbl = None
         lin = strx
-    X = re.split( r'\s' , lin.strip() )
+    X = re.split( '\s' , lin.strip() )
     for I in range(len(X)):
         x[I] = X[I].strip().upper()
     return lbl, x[0], x[1]
@@ -84,32 +112,34 @@ lblS = {}
 # Pass1  --> Enderecos de variaveis
 cntRAM = 0
 for L in cd:
-    lbl, opCode, valor = lSplit( L )
+    lbl, opCode, valor = lSplit( L.replace('\t', ' ') )
     if opCode in rWords:
-        lblS[lbl] = cntRAM
+        lblS[lbl.upper()] = cntRAM
         cntRAM = cntRAM + 1
     elif opCode in eWords:
         # EQU ou ORG ,
-        lblS[lbl] = valor
+        lblS[lbl.upper()] = valor
     elif lbl is not None:
-        lblS[lbl] = None  # No codigo, ainda nao sabemos o endereco ., 
+        lblS[lbl.upper()] = None  # No codigo, ainda nao sabemos o endereco ., 
 
 # Pass 2 -->
 # Enderecos das instrucoes
 cntPgm = 0
 passos = {}
 for L in cd:
-    lbl, opCode, valor = lSplit( L )
+    lbl, opCode, valor = lSplit( L.replace('\t', ' ')  )
     if opCode in rWords or opCode in eWords or opCode is None:
         # Alocacao de memoria RAM é so designativa
         continue  
     else:
         passos[cntPgm] = [ opCode, lbl , valor ]
         if lbl is not None:
-            lblS[lbl] = cntPgm  # Agora sabemos o endereco .. .
+            lblS[lbl.upper()] = cntPgm  # Agora sabemos o endereco .. .
         cntPgm = cntPgm + 1
 
 # Pass 3
+# dump known labels and values
+print( '\n'.join( '{} {}'.format( L, addr ) for L,addr in lblS.items() ) )
 
 FNhex = cdFonte.split('.')[0] + '.hex'
 FNlst = cdFonte.split('.')[0] + '.lst'
@@ -122,10 +152,9 @@ for L, (opCode, lbl , valor) in passos.items():
         operando = lblS[valor]
     else:
         operando = valor
-     
     codObj, sq, inst, op ,ciop = monta( L, opCode, operando )
     print(codObj, file=fhex)
-    print( codObj, sq, inst, op ,ciop, file=flst )
+    print( '{:02x}'.format(sq), codObj, sq, inst, op ,ciop, file=flst )
 
 fhex.close()
 flst.close()
